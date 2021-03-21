3f3d547766aa7ae413cbd90c24a1d61b70008160
unknown <emckenzie@zoology.ubc.ca>
2021-03-21 14:22:46 -0700

    working filter

--- sink_float.R
+++ sink_float.R
@@ -6,18 +6,20 @@ library(ggplot2)
 rm(larva_3)
 
 
-clean.dat <- X2021_03_12_larva_3 %>% 
+clean.dat <- X2021_03_12_larva_4 %>% 
   mutate(V = na_if(V, "#VALUE!")) %>% 
   mutate(cm = na_if(cm, "-"))
 
 clean.dat$cm <- as.numeric(clean.dat$cm)
 clean.dat$V <- as.numeric(clean.dat$V)
 
-  
-print(clean.dat, n= 220)
+clean.dat <- clean.dat %>% 
+  mutate(V = replace(V, V < -0.5, NA)) %>%
+  mutate(V = replace(V, V > 0.5, NA))
+
+print(clean.dat, n=400)
 view(clean.dat)
 
-
-ggplot(data = clean.dat, aes(x= s) +
-         geom_line(y= V)
+ggplot(data = clean.dat, aes(x= s, y= V), na.rm= T) +
+         geom_line()
        
