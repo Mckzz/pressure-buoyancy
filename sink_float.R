65abfdd2072bad4e5f426e0cfaecc07f28ad4515
unknown <emckenzie@zoology.ubc.ca>
2021-03-30 23:28:30 -0700

    filter progress

--- sink_float.R
+++ sink_float.R
@@ -1,23 +1,38 @@
 library(tidyverse)
 library(ggplot2)
 
-head(larva_1_velocities)
+head(larva_1_pressure_on)
 
-clean.dat <- larva_1_velocities %>% 
+clean.dat <- larva_2_pressure_off %>% 
   mutate(across(where(is.character), ~na_if(., "#VALUE!"))) %>%   #what do the tildes do?
   mutate(across(where(is.character), ~na_if(., "-")))%>%
   mutate(across(where(is.character), as.double))
 
 head(clean.dat)
+
 view(clean.dat)
 
 #velocity filter
 clean.dat <- clean.dat %>% 
-  mutate(V = replace(V, V < -0.11, NA)) %>%
-  mutate(V = replace(V, V > 0.11, NA))
+  mutate(Velocity_X = replace(Velocity_X, Velocity_X < -0.02, NA)) %>%
+  mutate(Velocity_X = replace(Velocity_X, Velocity_X > 0.02, NA)) %>% 
+  mutate(Velocity_Y = na_if(Velocity_X, NA)) %>% 
+  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y < -0.01, NA)) %>%
+  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y > 0.01, NA))
 
-print(clean.dat, n=400)
+# filter for net velocity as well?
+
+ggplot(data = clean.dat, aes(x= s, y= Velocity_Y), na.rm= T) +
+  geom_line()
+
+
+#### The subsequent net velocity seems to be much more reliably close to  
+#### the calculated y velocity than than the simultaneous net velocity...
+################ Its because I calculated using subsequent minus current, 
+################ not current minus previous
+
+
+print(clean.dat, n=100)
 view(clean.dat)
 
-ggplot(data = clean.dat, aes(x= s, y= V), na.rm= T) +
-         geom_line()
+
