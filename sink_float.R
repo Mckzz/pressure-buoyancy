4dc43dbe809f80fa679747323a1560491caa5179
unknown <emckenzie@zoology.ubc.ca>
2021-03-31 14:10:27 -0700

    filter problems solved

--- sink_float.R
+++ sink_float.R
@@ -3,27 +3,38 @@ library(ggplot2)
 
 head(larva_1_pressure_on)
 
-clean.dat <- larva_2_pressure_off %>% 
+clean.dat <- larva_1_pressure_off %>% 
   mutate(across(where(is.character), ~na_if(., "#VALUE!"))) %>%   #what do the tildes do?
   mutate(across(where(is.character), ~na_if(., "-")))%>%
   mutate(across(where(is.character), as.double))
 
-head(clean.dat)
-
-view(clean.dat)
-
 #velocity filter
 clean.dat <- clean.dat %>% 
-  mutate(Velocity_X = replace(Velocity_X, Velocity_X < -0.02, NA)) %>%
-  mutate(Velocity_X = replace(Velocity_X, Velocity_X > 0.02, NA)) %>% 
-  mutate(Velocity_Y = na_if(Velocity_X, NA)) %>% 
-  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y < -0.01, NA)) %>%
-  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y > 0.01, NA))
+  mutate(Velocity_X = replace(Velocity_X, Velocity_X < -0.05, NA)) %>%
+  mutate(Velocity_X = replace(Velocity_X, Velocity_X > 0.05, NA)) ## filter for very small Velocity_X values
+
+clean.dat$Velocity_Y[is.na(clean.dat$Velocity_X) == T] <- NA ##use the above filter to throw out dirty y velocity values
+
+## It would be nice if I could get this command into the above pipe using dplyr or something like that,
+## but the ] <- NA seems to mess with the pipe syntax. And I can't find a clear mutate option 
+## for replacing values in a column with NA based on whether the corresponding value in another column is NA
+
+clean.dat <- clean.dat %>% 
+  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y < -0.015, NA)) %>%
+  mutate(Velocity_Y = replace(Velocity_Y, Velocity_Y > 0.015, NA)) %>%
+  mutate(zeroline = 0)
+  #mutate(Velocity_Y = replace(Velocity_Y, Velocity_net > 0.01, NA))
+
+head(clean.dat)
+
+#view(clean.dat)
 
 # filter for net velocity as well?
 
-ggplot(data = clean.dat, aes(x= s, y= Velocity_Y), na.rm= T) +
-  geom_line()
+ggplot(data = clean.dat, aes(x= s), na.rm= T) +
+  geom_line(aes(y= Velocity_Y))+
+  geom_line(aes(y= zeroline), linetype = "dashed") +
+  theme_classic()
 
 
 #### The subsequent net velocity seems to be much more reliably close to  
